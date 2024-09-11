cb618a63472bdcb31ae5349240879245e732a14d
MaleneJensen1212 <Malense02@hotmail.com>
2024-09-11 14:56:26 +0200

    Made setup thingie

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#setup automatically --> Automate entering GitHub credentials
+git config --global user.email "Malense02@hotmail.com"
+git config --global user.name "MaleneJensen1212"
+#Run this when in the correct directory: bash setup.sh
