e5cacde9cd46e2f3033f29fce7e6d09e46e63ecb
MaleneJensen1212 <Malense02@hotmail.com>
2024-10-01 14:11:11 +0200

    lecture4

--- setup.sh
+++ setup.sh
@@ -2,4 +2,3 @@
 git config --global user.email "Malense02@hotmail.com"
 git config --global user.name "MaleneJensen1212"
 #Run this when in the correct directory: bash setup.sh
-#Access token from GitHub: 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
